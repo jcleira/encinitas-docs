f77fcedf9e5977cc9bfb3dc3a8363bf4ddc147f1
jcleira <jmc.leira@gmail.com>
2023-12-23 18:02:24 +0100

    Encinitas docs initial deployment
    * Objective
    
    This commit objective is about making the initial Encinitas
    documentation deployment.
    
    * Why
    
    Everything has a beginning, so here we go.
    
    * How
    
    This commit updates the default docusaurus template with the Home Page
    for encinitas.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,18 +13,57 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+        All-in-one observability tool for Solana dApps
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
+  );
+}
+
+function IntroSection() {
+  return (
+    <section className={styles.introSection}>
+      <div className="container">
+        <h2>
+        Start using Encinitas
+        </h2>
+        <h3>
+      Encinitas is an observability platform that helps you build better dApps. You can bring in data from any Smart Contract and their frontend so that you can fully understand your system, analyze that data efficiently, and respond to incidents before they become problems. As extensive as the capablities of Encinitas are, you can get started with the platform by following a three-step procedure.
+        </h3>
+      </div>
+    </section>
+  );
+}
+
+function Step({ stepNumber, stepTitle, children }) {
+  return (
+    <div className={styles.step}>
+      <h3 className={styles.stepTitle}>
+        STEP {stepNumber} OF 3
+      </h3>
+      <h4 className={styles.stepAction}>
+        {stepTitle}
+      </h4>
+      {children}
+    </div>
+  );
+}
+
+function StepsSection() {
+  return (
+    <section className={styles.stepsSection}>
+      <div className="container">
+        <Step stepNumber="1" stepTitle="Configure Your Smart Contracts">
+          <p>Set up your Solana Smart Contracts for integration and prepare your dApp for observability.</p>
+        </Step>
+        <Step stepNumber="2" stepTitle="Ingest Your Data">
+          <p>Encinitas auto-discovers your dApp configuration, guiding you through the setup. Once infrastructure agent is ready it will start collecting data.</p>
+        </Step>
+        <Step stepNumber="3" stepTitle="Explore Dashboards">
+          <p>With data incoming, delve into dashboards for real-time insights. If you encounter issues, refer to our help resources.</p>
+        </Step>
+      </div>
+    </section>
   );
 }
 
@@ -32,10 +71,12 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="All-in-one observability tool for Solana dApps">
       <HomepageHeader />
       <main>
+        <IntroSection/>
+        <StepsSection/>
         <HomepageFeatures />
       </main>
     </Layout>
